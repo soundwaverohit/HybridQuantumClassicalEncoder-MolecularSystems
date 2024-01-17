ab1f15836024ad9d13bc1e0bdacc3b1e8f133031
Rohit Ganti <soundwave.rohit@gmail.com>
2024-01-17 08:49:09 -0500

    Update executor.py

--- executor.py
+++ executor.py
@@ -8,15 +8,20 @@ from classical_decoder import decoder
 
 # Sample input data
 input_data = torch.rand(20)  # Replace with real data
+print("sample data:", input_data)
 
 # Encode the input data
 encoded_params = encoder(input_data)
+print("The encoded parameters are: ", encoded_params)
+
 
 # Run the quantum circuit
 quantum_circuit_output = run_quantum_circuit(encoded_params)
+print(quantum_circuit_output)
 
 # Decode the output of the quantum circuit
 decoded_output = decoder(quantum_circuit_output)
 
 # Print the final output
+print("The final output after decoding is: ")
 print(decoded_output)
